5345e89cdaa409bbe5c6f360e42244e005ded59e
ldav99 <davidson.139@wright.edu>
2025-03-13 16:49:03 -0400

    update

--- taskA-4.py
+++ taskA-4.py
@@ -27,17 +27,21 @@ def imageFiltering(filter, image):
     outputHeight, outputWidth = imageHeight-len(filter)+1 , imageWidth-len(filter)+1 #394 294
 
     #Create output array with the correct dimensions
-    output = np.zeros([imageHeight, imageWidth])
-    tempImg = np.zeros([imageHeight, imageWidth])
-    pixelGroup = np.zeros([7,7])
+    output = np.zeros([imageHeight, imageWidth], dtype=int)
+    tempImg = np.zeros([imageHeight, imageWidth], dtype=int)
+    pixelGroup = np.zeros([7,7], dtype=int)
     
-
+#Itterate over the original image and take a group of 7x7 pixels to be used 
     for y in range(imageHeight):
         for x in range(imageWidth):
             tempImg[y, x] = image[y, x]
-            np.append(pixelGroup , tempImg[y:y+7 , x:x+7])
 
-            output.append(sum(np.dot(pixelGroup, filter)))
+#Slide the grid till the edge hits the edges of the image 
+            if x <= (imageWidth - 7) or y <= (imageHeight - 7):
+                np.append(pixelGroup , tempImg[y:y+7 , x:x+7])#Assign this to the pizelGroup
+
+#Take the sum of the pixel group multiplied by the filter and append it to the output 
+            np.append(output, sum(np.dot(pixelGroup, filter)))
 
     print(tempImg)
     print('--------')
