6936df68235de4cfa3bb1fde8c1b964aa2c72d62
ldav99 <davidson.139@wright.edu>
2025-03-16 14:38:11 -0400

    Updated taskA-4. convolution now "works"

--- taskA-4.py
+++ taskA-4.py
@@ -5,11 +5,12 @@
 
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 
 
 def main():
     #Load Image
-    image = "Images/creeper.png"
+    image = "Images/image3.jpg"
     #Read an Image of your choice
     originalImage = cv2.imread(image)
 
@@ -23,29 +24,28 @@ def main():
 
 
 def imageFiltering(filter, image):
+#Get original image and output image height and width
     imageHeight, imageWidth = image.shape
-    outputHeight, outputWidth = imageHeight-len(filter)+1 , imageWidth-len(filter)+1 #394 294
+    outputHeight, outputWidth = imageHeight-len(filter)+1 , imageWidth-len(filter)+1 #394 294 or 26,26
 
-    #Create output array with the correct dimensions
-    output = np.zeros([imageHeight, imageWidth], dtype=int)
-    tempImg = np.zeros([imageHeight, imageWidth], dtype=int)
-    pixelGroup = np.zeros([7,7], dtype=int)
+#Create output array with the correct dimensions
+    output = np.zeros((outputHeight, outputWidth))
     
 #Itterate over the original image and take a group of 7x7 pixels to be used 
-    for y in range(imageHeight):
-        for x in range(imageWidth):
-            tempImg[y, x] = image[y, x]
+    for y in range(outputHeight):
+        for x in range(outputWidth):
+            tempImg = np.array(image)
 
 #Slide the grid till the edge hits the edges of the image 
             if x <= (imageWidth - 7) or y <= (imageHeight - 7):
-                np.append(pixelGroup , tempImg[y:y+7 , x:x+7])#Assign this to the pizelGroup
+                pixelGroup = tempImg[y:y+7, x:x+7]
 
 #Take the sum of the pixel group multiplied by the filter and append it to the output 
-            np.append(output, sum(np.dot(pixelGroup, filter)))
-
-    print(tempImg)
-    print('--------')
-    print(pixelGroup)
+            output[y, x] = np.sum(np.dot(filter , pixelGroup))
+            
+#Use Pyplot to take the output array of values and show them as an image
+    plt.imshow(output, interpolation='nearest')
+    plt.show()
 
     return output
 
